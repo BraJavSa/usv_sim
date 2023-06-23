a84d67b3924e882b7cc6b0e2cd4b0b53f32d64b1
BraJavSa <brayanjavsa@gmail.com>
2023-06-22 21:58:11 -0300

    update polar control

--- scripts/polar_position_control.py
+++ scripts/polar_position_control.py
@@ -11,16 +11,16 @@ class PositionController:
         self.wRef=0
         self.hxe=0
         self.hye=0
-        self.a=0.01 #punto de interes
+        self.a=0.1 #punto de interes
         self.current_x=0
         self.current_y=0
         self.orientation=0
         self.current_heading=0
         #kv= u max,    wmax= kk*pi + kv*0.5
 
-        self.kv=2
-        self.kk=0.159
-        
+        self.kv=1.5
+        self.kk=0.1
+        self.k2=0.1
         self.odom_subscriber = rospy.Subscriber('/boat/odom', Odometry, self.odom_callback)
         self.vel_publisher = rospy.Publisher('/boat/cmd_vel', Twist, queue_size=10)
         self.rate = rospy.Rate(10)
@@ -38,17 +38,29 @@ class PositionController:
         
         error=math.sqrt(self.hxe**2 + self.hye**2)
         a_e= math.atan2(self.hye, self.hxe)-self.orientation
-        if error <=0.1:
+
+        if error <=10:
+            self.kv=0.3
+            self.kk=0.1
+        else:
+            self.kv=1.5
+            self.kk=0.1
+
+
+        if error <=1:
             self.uRef=0
             self.wRef=0
         
         else:
-            self.uRef=self.kv*math.tanh(error)*math.cos(a_e)
-            self.wRef=self.kk*a_e+self.kv*(math.tanh(error)/error)*math.sin(a_e)*math.cos(a_e)
-
+            self.uRef=self.kv*math.tanh(self.k2*error)*math.cos(a_e)
+            self.wRef=self.kk*a_e+self.kv*(math.tanh(self.k2*error)/error)*math.sin(a_e)*math.cos(a_e)
+ 
         twist_msg = Twist()
         twist_msg.linear.x = self.uRef
         twist_msg.angular.z = self.wRef
+        
+
+        print(str(self.uRef)+","+str(self.wRef)+", a_e: "+str(a_e)+", error:"+str(error))
         self.vel_publisher.publish(twist_msg)
         self.rate.sleep()
 
